3916ed57797eac99f120847c4105bd2270b050b8
filipe <felepesilvagalvao@gmail.com>
2026-03-19 13:57:05 -0300

    listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import styles from "./page.module.css"
 export default function Home() {
   return (
-    <div>
-      <h1>Next ecommerce</h1>
+    <div className={styles.page__container}>
+      <div className={styles.page__grid}>
+        <div>
+          1
+        </div>
+        <div>
+          2
+        </div>
+        <div>
+          3
+        </div>
+        <div>
+          4
+        </div>
+      </div>
     </div>
   );
 }
