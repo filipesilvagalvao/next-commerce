87c3c27db6a4710852febdc8f3ab14598167837d
filipe <felepesilvagalvao@gmail.com>
2026-03-19 10:50:02 -0300

    adicionando navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <Link href="/">Next Store</Link>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import NavBar from "./components/NavBar";
 
 export const metadata: Metadata = {
   title: "Ecommerce",
@@ -23,8 +13,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="pt-br">
+      <body>
+        <NavBar/>
+        <main className="main">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
